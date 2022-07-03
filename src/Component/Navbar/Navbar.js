b9f244027011105d98cf5957e96325d82547e21b
Angad-Godara <angadrrr2002@gmail.com>
2022-07-03 12:03:57 +0530

    ADD: Navbar completed

---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Navbar.css'
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+function Navbar() {
+    return (
+        <div className='nav__bar'>
+            <img className='logo' src='https://pngimg.com/uploads/amazon/amazon_PNG11.png' alt='logo' />
+            <div className='nav__search'>
+                <input className='search__input' type='text' />
+                <SearchIcon className='search__icon' />
+            </div>
+            <div className='option__container'>
+                <div className='nav__options'>
+                    <div className='upper'>
+                        <span>Hello Guest</span>
+                    </div>
+                    <div className='lower'>
+                        <span>Sign IN</span>
+                    </div>
+                </div>
+                <div className='nav__options'>
+                    <div className='upper'>
+                        <span>Returns</span>
+                    </div>
+                    <div className='lower'>
+                        <span>& Orders</span>
+                    </div>
+                </div>
+                <div className='nav__options'>
+                    <div className='upper'>
+                        <span>Your</span>
+                    </div>
+                    <div className='lower'>
+                        <span>Prime</span>
+                    </div>
+                </div>
+                <div className='cart'>
+                    <ShoppingCartIcon />
+                    <span className='items'>6</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
